bcb0313a589d5fa040693896f9dd18be9173d127
sunrise-rab <rabia.tsdi@gmail.com>
2026-01-16 21:43:56 +0000

    added secret key to settings

--- bead_bond/settings.py
+++ bead_bond/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uyf@q$wq=@)ke#y2t2(_-=3aq_-#dtqqhtos10#bb(z0%)r!67'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
