389be24a0bfeb2feef5763f675cd71044d01bb2b
sunrise-rab <rabia.tsdi@gmail.com>
2026-01-18 02:50:22 +0000

    Fix static files for Heroku

--- bead_bond/settings.py
+++ bead_bond/settings.py
@@ -160,6 +160,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR / "static"),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
